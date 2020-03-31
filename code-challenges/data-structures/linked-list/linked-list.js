73e3c41424459fe9cca095e66ab48a75d95b5e82
dannevernguyens <danguyenuw@gmail.com>
2020-03-30 19:33:52 -0700

    getting some files setup for class 5 lab

---
+++ code-challenges/data-structures/linked-list/linked-list.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/*
+create two classes: Node and Linkedlist
+both exported by this file
+/////////////
+Node should have these properties: val, next, prev
+// no functions or methods needed
+LinkedList should have these properties: head
+// functions|methods: insert(), includes(), toString()
+/////////////
+Tests: need to write 7 tests
+*/
